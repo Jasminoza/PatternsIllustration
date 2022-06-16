2b678eb6935294162de9218cab2fbde8666dbd1b
Jasminoza <testbot91@gmail.com>
2022-06-16 14:58:40 +0300

    The chain pattern was refactored.

--- src/main/java/behavioral/chain/OrderTracker.java
+++ src/main/java/behavioral/chain/OrderTracker.java
@@ -11,5 +11,19 @@ public class OrderTracker {
         workerNotifier.sendMessages(OrderPriority.NORMAL);
         System.out.println();
         workerNotifier.sendMessages(OrderPriority.HIGH);
+        System.out.println("==========================================================\n");
+
+        managerNotifier.sendMessages(OrderPriority.LOW);
+        System.out.println();
+        managerNotifier.sendMessages(OrderPriority.NORMAL);
+        System.out.println();
+        managerNotifier.sendMessages(OrderPriority.HIGH);
+        System.out.println("==========================================================\n");
+
+        bossNotifier.sendMessages(OrderPriority.LOW);
+        System.out.println();
+        bossNotifier.sendMessages(OrderPriority.NORMAL);
+        System.out.println();
+        bossNotifier.sendMessages(OrderPriority.HIGH);
     }
 }
